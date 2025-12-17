303982cdb00fd42dd12fc1e0d024a1fdea6cefca
JosephineScharf <eaa25jotz@students.eaaa.dk>
2025-12-17 12:24:13 +0100

    first page

---
+++ exam_project/assets/js/javascript.js
@@ -0,0 +1,8 @@
+
+const menuBtn = document.getElementById("menuBtn");
+  const burgerMenu = document.getElementById("burgerMenu");
+
+  menuBtn.addEventListener("click", () => {
+    burgerMenu.classList.toggle("open");
+  });
+
